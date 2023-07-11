35f039adf7d81466059859c0caa49596984b4077
Ben <62263898+BenBissTech@users.noreply.github.com>
2023-07-11 18:55:28 +0100

    Added routing functionality
    Imported necessary modules:
    
    BrowserRouter - stores current location in the browser's address bar using clean URLs
    
    Bootstrap.min.csss - boostrap (popular CSS framework) minified, all whitespace and extra characters removed
    
    Form - wrapper component, emulates the browser for client side routing and data mutation
    
    Added re-routing functioanlity for / and /app - will route to the singular webpage

--- url-short/src/App.js
+++ url-short/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+import Form from "./components/Form";
+// shift + alt + f to format
+// Two routes to route to the home form page, regardless if just / or /app
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <switch>
+            <Route exact path='/' component={Form} />
+            <Route path="/app" component={Form} />
+          </switch>
+        </div>
+      </div>
     </div>
   );
 }
