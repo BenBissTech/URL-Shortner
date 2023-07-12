d1bcb3e12e695752d42290471d3d88d6f73cb8c2
Ben <62263898+BenBissTech@users.noreply.github.com>
2023-07-12 17:31:21 +0100

    Created Form.js - collect user input - main webpage functionality
    Created components folder with form.js
    
    Added necessary imports:
    
    React
    Nanoid - generate valid URL (no special characters, spaces, etc.)
    Firebase/database - firebase database functionality
    isWebUrl - validates if user inputted URL is valid
    OverlayTrigger and Tooltip - will add copied to clipboard functionality

---
+++ url-short/src/components/Form.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { nanoid } from 'nanoid';
+import { getDatabase, child, ref, set, get } from 'firebase/database';
+import { isWebUrl } from 'valid-url'
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootsrap/Tooltip";
+
+
