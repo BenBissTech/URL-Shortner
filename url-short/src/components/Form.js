065409683389f9dc347d568bdcf1ee2f8c609c74
Ben <62263898+BenBissTech@users.noreply.github.com>
2023-07-12 18:43:30 +0100

    Basic form and database functionality
    Added simple user input and database storage functionality

--- url-short/src/components/Form.js
+++ url-short/src/components/Form.js
@@ -33,4 +33,52 @@ class Form extends React.Component {
         };
     }
 
+    // onSubmit handles user event - user click
+    onSubmit = async (event) => {
+        // used to prevent default of refreshing page upon suer click
+        event.preventDefault();
+        // sets loading state to true and resets generated URL to blank (in cases of multiple use)
+        this.setState({
+            loading: true,
+            generatedURL: ''
+        })
+
+        // calls method validateInput() to verifiy submitted user input is in a valid form
+        var isFormValid = await this.validateInput()
+        if (!isFormValid) {
+            return
+        }
+
+        // generate alias for short URL and attaches it to domain
+        var generatedAlias = nanoid(5)
+        var generatedURL = "shrinklink.com/" + generatedAlias;
+
+        // check if user has inputted a prefered alias
+        if (this.state.preferedAlias !== '') {
+            generatedAlias = this.state.preferedAlias;
+            generatedURL = "shrinklink.com/" + this.state.preferedAlias;
+        }
+
+        /* Database Functionality */
+        // ref to database
+        const db = getDatabase();
+        // set/write data to database, db = database reg, '/' + generatedAlias = key
+        set(ref(db, '/' + generatedAlias), {
+            generatedAlias: generatedAlias,
+            longURL: this.state.longURL,
+            preferedAlias: this.state.preferedAlias,
+            gerneatedURL: generatedURL
+        }).then((result) => {
+            // once data is stored, set current state of component to generated URL stored and loading to false as database operations are complete
+            this.setState({
+                generatedURL: generatedURL,
+                loading: false
+            })
+        // error handling
+        }).catch((e) => {
+
+        })
+
+
+
 }
