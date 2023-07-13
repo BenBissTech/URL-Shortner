14e67bc3eba75a85b3293610ed3f26536c10d85e
Ben <62263898+BenBissTech@users.noreply.github.com>
2023-07-13 18:55:19 +0100

    Added user functionality
    Added error checking on preferred alias
    
    Added html code for web page functionality

--- url-short/src/components/Form.js
+++ url-short/src/components/Form.js
@@ -117,7 +117,167 @@ class Form extends React.Component {
 
 
             // Perfered Alias validation
-            
+            // checks if user has added an alias
+            if (this.state.preferedAlias !== '') {
+                // alias should be less than 7 characters
+                if (this.state.preferedAlias.length > 7) {
+                    error.push("preferedAliass");
+                    errorMessages['preferedAlias'] = 'Please enter an alias less than 7 characters';
+                }
+                // alias should not have any spaces
+                else if (this.state.preferedAlias.indexOf(' ') >= 0) {
+                    errors.push("preferedAlias");
+                    errorMessage['preferedAlias'] = 'Please remove all spaces';
+                }
+
+
+                // check if alias provided already exists
+                var keyExists = await this.checkKeyExists()
+
+                if (keyExists.exists()) {
+                    error.push("preferedAlias");
+                    errorMessage['preferedAlias'] = 'Please enter a different alias, alias already exists';
+                }
+            }
+
+            // update state of component
+            this.setState({
+                errors: errors,
+                errorMessages: errorMessages,
+                loading: false
+            });
+
+            // checks if errors are present
+            if (errors.length > 0) {
+                return false;
+            }
+
+            return true;
+        }
+
+        // method to check if alias already exists
+        checkKeyExists = async () => {
+            const dbRef = ref(getDatabase());
+            var result = get(child(dbRef, '/${this.state.preferedAlias}')).catch((error) => { return false });
+            return result;
+        }
+
+        // method to copy generated URL to clipboard
+        copyToClipBoard = () => {
+            navigator.clipboard.writeText(this.state.generatedURL)
+            this.setState({
+                toolTipMessage: 'Copied to clipboard!'
+            })
+        }
+
+        // method to render HTML code
+        render() { 
+            return (
+                <div className="container">
+                    <form autoComplete="off">
+                        <h3>Shrink Link</h3>
+        
+                        <div className="form-group">
+                            <label>Enter your long URL</label>
+                            <input
+                                id="longURL"
+                                // user input - handle change
+                                onChange={this.handleChange}
+                                value={this.state.longURL}
+                                type="url"
+                                required
+                                // checks if error array has value longURL
+                                className={
+                                    this.hasError("longURL")
+                                        // if error, show form is invalid
+                                        ? "form-control is-invalid"
+                                        // otherwise carry on
+                                        : "form-control"
+                                }
+                                // placeholder for user input box                      
+                                placeholder="https://www..."
+                            />
+                        </div>
+
+                        <div
+                            // hide error message until state has changed (user clicked button)
+                            className={
+                                this.hasError("longURL")
+                                    ? "text-danger" : "visually-hidden"
+                            }
+                        >
+                            {this.state.errorMessage.longURL}
+                        </div>
+                        
+
+                        <div className="form-group">
+                            <label html="basic-url">Your Short Link</label>
+                            <div className="input-group mb-3">
+                                <div className="input-group-prepend">
+                                    <span cclassName="input-group-text">shrinklink.com/</span>
+                                </div>
+                                <input
+                                    id="preferedAlias"
+                                    onChange={this.handleChange}
+                                    value={this.state.preferedAlias}
+                                    className={
+                                        this.hasError("preferedAlias")
+                                            ? "form-control"
+                                            : "form-control"
+                                    }
+                                    type="text" placeholder="eg. 8f2asd"
+                                />
+                            </div>
+                            <div
+                                className={
+                                    this.hasError("preferedAlias") ? "text-danger" : "visually-hidden"
+                                }
+                            >
+                                {this.state.errorMessage.preferedAlias}
+                            </div>
+                        </div>
+
+                        <button className="btn btn-primary" type="button" onClick={this.onSubmit}>
+                            {
+                                // if state is loading show spinner button, if not hide spinner
+                                this.state.loading ?
+                                <div>
+                                    <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                                </div> :
+                                <div>
+                                    <span className="visually-hidden spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                                    <span>Shrink Link</span>
+                                </div>
+                            }
+                        </button>
+
+                        {
+                            <div className="generatedurl">
+                                <span>Your generated URL is: </span>
+                                <div className="input-group mb-3">
+                                    <input disabled type="text" value={this.state.generatedURL} className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                                    <div className="input-group-append">
+                                        <OverlayTrigger
+                                            key={'top'}
+                                            placement={'top'}
+                                            overlay={
+                                                <Tooltip id={`tooltip-${'top'}`}>
+                                                    {this.state.toolTipMessage}
+                                                </Tooltip>
+                                            }
+                                        >
+                                            <button onClick={() => this.copyToClipBoard()} data-toggle="tooltip" data-placement="top" title="Tooltip on top" className="btn btn-outline-secondary" type="button">Copy</button>
+                                        </OverlayTrigger>
+
+                                    </div>
+                                </div>
+                            </div>
+                        }
+                    </form>
+                </div>
+            );
         }
     }
 }
+
+export default form;
